8058e249e1fd34a2da1c43866d1b41dd35e26728
Thunx <friberg_mattias@hotmail.com>
2011-12-05 22:55:02 +0100

    Fix for Thunxtoggletext

--- ThunxToggleText/core.lua
+++ ThunxToggleText/core.lua
@@ -1,102 +1,28 @@
-local E, C, L, DB = unpack(select(2, ...)) -- Import Functions/Constants, Config, Locales
-local E, C, L, DB = unpack(ElvUI)
---ttest
-TC = {	
-	fontsize = 12,								-- Font storlek
-	font = "Interface\\AddOns\\ThunxToggleText\\PT_Sans_Narrow.ttf",	-- Font
-	textcolor = "|cff1784d1",						-- Text Color
-	--Omen
-	Omen = true,
-	OposX = E.Scale(-200),								-- Position i x-led
-	OposY = E.Scale(3),									-- Position i y-led (ökar värdet så går den längre ner på skärmen)
-	
-	--Recount
-	Recount = true,
-	RposX = E.Scale(-150),								-- Position i x-led
-	RposY = E.Scale(3),									-- Position i y-led (ökar värdet så går den längre ner på skärmen)
-	
-	--Atlasloot
-	Atlasloot = true,
-	AposX = E.Scale(-100),								-- Position i x-led
-	AposY = E.Scale(3),									-- Position i y-led (ökar värdet så går den längre ner på skärmen)
-	
-	--Skada
-	Skada = true,
-	SposX = E.Scale(-150),								-- Position i x-led
-	SposY = E.Scale(3),									-- Position i y-led (ökar värdet så går den längre ner på skärmen)
-	
-	
-	} 
---------------------------------------------------------------------
------Rör inget här under-----
---------------------------------------------------------------------
+local E, L, DF = unpack(ElvUI); -- Import Functions/Constants, Config, Locales
 
---------------------------------------------------------------------
------Omen toggle knapp-----
---------------------------------------------------------------------
-if IsAddOnLoaded("Omen") and TC.Omen == true then
-	--[[function E.PostOmenMove(frame)
-		OmenMoveIndicator:ClearAllPoints()
-		OmenMoveIndicator:SetPoint("BOTTOMRIGHT", ChatRPlaceHolder, "TOPRIGHT", -200, 20)
-	end
-	local once = false
-	hooksecurefunc(OmenMoveIndicator, "SetPoint", function(_,_,parent)    ]]
-	
-	
-	local OmenText = CreateFrame("Button","rOmenShowHide", ChatRBGTab, "SecureHandlerStateTemplate")
-	
-	OmenText:ClearAllPoints()
-	OmenText:SetPoint("BOTTOMRIGHT", ChatRPlaceHolder, "TOPRIGHT", TC.OposX, TC.OposY)
-	OmenText:SetWidth(50) OmenText:SetHeight(25)
-	OmenText.text= OmenText:CreateFontString(nil,"OVERLAY","GameFontNormalSmall")
-	
-	OmenText.text:SetPoint("CENTER")
-	OmenText.text:SetFont(TC.font, TC.fontsize, "THINOUTLINE")
-	OmenText.text:SetText(TC.textcolor.."Omen")
-	
-	
-	OmenText:SetScript("OnMouseDown", function(self, btn)
-		if btn == "RightButton" then Omen:ShowConfig() else Omen:Toggle()		
-		end 
-	end)
-	
-	if IsAddOnLoaded("ElvUI") then
-		local x = CreateFrame("Frame")
-		x:RegisterEvent("PLAYER_ENTERING_WORLD")
-		x:SetScript("OnEvent", function(self, event)
-		 if not Omen then return end
-		 ChatRBG:HookScript("OnShow", function() Omen:Toggle(false) OmenText:Show(); end)
-		 ChatRBG:HookScript("OnHide", function() Omen:Toggle(true) OmenText:Hide(); end)
-		 self:UnregisterAllEvents()
-		end)
-	end
-	E.CreateMover(OmenText, "OmenTextMover", "oMOVE")
-end
 
 --------------------------------------------------------------------
 -----Recount toggle knapp-----
 --------------------------------------------------------------------
-if IsAddOnLoaded("Recount") and TC.Recount == true then
-
-	local RecountText=CreateFrame("Button", "RecountShowHide", ChatRBGTab, "SecureHandlerStateTemplate" )
+	local RecountText = CreateFrame('Button', 'RText', RightChatPanel) 
+	RecountText:Point("TOPLEFT",  RightChatPanel, "TOPRIGHT", -100, -4)
+	RecountText:Size( 50, 22)
+	RecountText:SetFrameLevel(10)
 	
-	--RecountText:SetTemplate("Default", false)
-	RecountText:ClearAllPoints()
-	RecountText:SetPoint("BOTTOMRIGHT", ChatRPlaceHolder, "TOPRIGHT", TC.RposX, TC.RposY)
-	RecountText:SetWidth(50) RecountText:SetHeight(25)
+	RecountText.text = RecountText:CreateFontString(nil, 'OVERLAY')
+	RecountText.text:FontTemplate()
+	RecountText.text:SetPoint('CENTER')
+	RecountText.text:SetJustifyH('CENTER')
+	RecountText.text:SetText(E.ValColor..'Recount')
 	
-	RecountText.text = RecountText:CreateFontString(nil,"OVERLAY","GameFontNormalSmall")
-	--RecountText:FontString(nil, C["media"].font, C["general"].fontscale, "THINOUTLINE")
-	RecountText.text:SetPoint("CENTER")
-	RecountText.text:SetFont(TC.font, TC.fontsize, "THINOUTLINE")
-	RecountText.text:SetText(TC.textcolor.."Recount")
-
+	E.CreateMover(anchor, RecountText, "RecountTextMover", "rMOVE")
+	
 	RecountText:SetScript("OnMouseDown", function(self, btn)
 		if btn == "RightButton" then Recount:ShowConfig()
 		else
 			if(Recount.MainWindow:IsShown())then Recount.MainWindow:Hide()
-			else Recount.MainWindow:Show() Recount:RefreshMainWindow() 
-			end 
+			else Recount.MainWindow:Show() Recount:RefreshMainWindow()
+			end
 		end
 	end)
 	
@@ -104,116 +30,82 @@ if IsAddOnLoaded("Recount") and TC.Recount == true then
 		local x = CreateFrame("Frame")
 		x:RegisterEvent("PLAYER_ENTERING_WORLD")
 		x:SetScript("OnEvent", function(self, event)
-		 if not Recount then return end
-		 ChatRBG:HookScript("OnShow", function() Recount.MainWindow:Hide() RecountText:Show(); end)
-		 ChatRBG:HookScript("OnHide", function() Recount.MainWindow:Show() Recount:RefreshMainWindow()  RecountText:Hide(); end)
-		 self:UnregisterAllEvents()
+		if not Recount then return end
+			RightChatPanel:HookScript("OnShow", function() Recount.MainWindow:Hide() RecountText:Show(); end)
+			RightChatPanel:HookScript("OnHide", function() Recount.MainWindow:Show() Recount:RefreshMainWindow() RecountText:Hide(); end)
+			self:UnregisterAllEvents()
 		end)
 	end
-	E.CreateMover(RecountText, "RecountTextMover", "rMOVE")
-end
-
 --------------------------------------------------------------------
------Atlasloot toggle knapp-----
+-----Omen toggle knapp-----
 --------------------------------------------------------------------
-if IsAddOnLoaded("AtlasLoot_Loader") and TC.Atlasloot == true  then
+	local OmenText = CreateFrame('Button', 'OText', RightChatPanel) 
+	OmenText:Point("TOPLEFT",  RightChatPanel, "TOPRIGHT", -150, -4)
+	OmenText:Size( 50, 22)
+	OmenText:SetFrameLevel(10)
+	--OmenText:SetTemplate('Default', true)
 	
-	local AtlasText = CreateFrame("Button", "AtlasShowHide", ChatRBGTab, "SecureHandlerStateTemplate")
+	OmenText.text = OmenText:CreateFontString(nil, 'OVERLAY')
+	OmenText.text:FontTemplate()
+	OmenText.text:SetPoint('CENTER')
+	OmenText.text:SetJustifyH('CENTER')
+	OmenText.text:SetText(E.ValColor..'Omen')
 	
-	AtlasText:ClearAllPoints()
-	AtlasText:SetPoint("BOTTOMRIGHT", ChatRPlaceHolder, "TOPRIGHT", TC.AposX, TC.AposY)
-	AtlasText:SetWidth(50) AtlasText:SetHeight(25)
-	
-	AtlasText.text = AtlasText:CreateFontString(nil,"OVERLAY","GameFontNormalSmall")
-	AtlasText.text:SetPoint("CENTER")
-	AtlasText.text:SetFont(TC.font, TC.fontsize, "THINOUTLINE")
-	AtlasText.text:SetText(TC.textcolor.."Atlas")
-	
-	AtlasText:SetScript("OnMouseDown", function(self, btn)
-		if btn == "RightButton" then AtlasLoot:OptionsToggle()
-		else 
-			AtlasLoot:ShowFrame_MiniMap()
-		end 
-	end)
-	
-	if IsAddOnLoaded("ElvUI") then
-		local x = CreateFrame("Frame")
-		x:RegisterEvent("PLAYER_ENTERING_WORLD")
-		x:SetScript("OnEvent", function(self, event)
-		 if not Recount then return end
-		 ChatRBG:HookScript("OnShow", function() AtlasText:Show(); end)
-		 ChatRBG:HookScript("OnHide", function() AtlasText:Hide(); end)
-		 self:UnregisterAllEvents()
-		end)
-	end
-	E.CreateMover(AtlasText, "AtlasTextMover", "aMOVE")
-end
---------------------------------------------------------------------
------Skada toggle knapp-----
---------------------------------------------------------------------
-if IsAddOnLoaded("Skada") and TC.Skada == true then
-	
-	local SkadaText = CreateFrame("Button", "SkadaShowHide", ChatRBGTab, "SecureHandlerStateTemplate")
-	SkadaText:SetPoint("BOTTOMRIGHT", ChatRPlaceHolder, "TOPRIGHT", TC.SposX, TC.SposY)
-	SkadaText:SetWidth(50) SkadaText:SetHeight(25)
-	
-	SkadaText.text=SkadaText:CreateFontString(nil,"OVERLAY","GameFontNormalSmall")
-	SkadaText.text:SetPoint("CENTER")
-	SkadaText.text:SetFont(TC.font, TC.fontsize, "THINOUTLINE")
-	SkadaText.text:SetText(TC.textcolor.."Skada")
-	
-	SkadaText:SetScript("OnMouseDown", function(self, btn)
-		if btn == "RightButton" then InterfaceOptionsFrame_OpenToCategory("Skada") else Skada:ToggleWindow()
-			
-		end 
-	end)
-	
-	if IsAddOnLoaded("ElvUI") then 
-		local x = CreateFrame("Frame")
-		x:RegisterEvent("PLAYER_ENTERING_WORLD")
-		x:SetScript("OnEvent", function(self, event)
-		if not Skada then return end
-		ChatRBG:HookScript("OnShow", function() SkadaText:Show(); Skada:SetActive(false) end)
-		ChatRBG:HookScript("OnHide", function() SkadaText:Hide(); Skada:SetActive(true) end)
-		self:UnregisterAllEvents()
-		end)
-	end
-	E.CreateMover(SkadaText, "SkadaTextMover", "sMOVE")	
-end
-
-if ElvUI then
-	
-elseif TukUI then
---	T.CreateMover(TukUI_TrinketBar, "TukUI_TrinketBar", "Trink Bar")
-end
---------------------------------------------------------------------
------Test toggle knapp-----
---------------------------------------------------------------------
---[[ 
-if IsAddOnLoaded("Omen") then
-	local OmenText=CreateFrame("Button","OmenShowHide",UIParent)
-
-	OmenText:SetPoint("TOP", UIParent, "TOP", TC.OposX, TC.OposY)
-
-	OmenText:SetWidth(50) OmenText:SetHeight(25)
-	OmenText:SetBackdrop({bgFile="Interface\AddOns\ElvUI\media\textures\blank",
-					insets = {left = 5, right = 5, top = 5, bottom = 5}} )
-
-	OmenText.text=OmenText:CreateFontString(nil,"OVERLAY","GameFontNormalSmall")
-	OmenText.text:SetPoint("CENTER",0,0)
-	OmenText.text:SetFont(TC.font, TC.fontheight)
-	OmenText.text:SetText(TC.textcolor.."Omen")
-	
-	if IsAddOnLoaded("ElvUI") then
-		if E.myname == "Tanix" then
-		OmenText.text:SetText(E.ValColor.."Omen")
-		end
-	end
+	E.CreateMover(anchor, OmenText, "OmenTextMover", "oMOVE")
 	
 	OmenText:SetScript("OnMouseDown", function(self, btn)
-		if btn == "RightButton" then Omen:ShowConfig() else Omen:Toggle()		
-		end 
+		if btn == "RightButton" 
+			then 
+				Omen:ShowConfig() 
+			else 
+				Omen:Toggle()
+		end
 	end)
-end 
-]]
-
+	
+	if IsAddOnLoaded("ElvUI") then
+		local x = CreateFrame("Frame")
+		x:RegisterEvent("PLAYER_ENTERING_WORLD")
+		x:SetScript("OnEvent", function(self, event)
+		if not Recount then return end
+			RightChatPanel:HookScript("OnShow", function() Omen:Toggle(false) OmenText:Show(); end)
+			RightChatPanel:HookScript("OnHide", function() Omen:Toggle(true) OmenText:Hide(); end)
+			self:UnregisterAllEvents()
+		end)
+	end
+--------------------------------------------------------------------
+-----AtlasLoot toggle knapp-----
+--------------------------------------------------------------------
+	local AtlasText = CreateFrame('Button', 'AText', RightChatPanel) 
+	AtlasText:Point("TOPLEFT",  RightChatPanel, "TOPRIGHT", -200, -4)
+	AtlasText:Size( 50, 22)
+	AtlasText:SetFrameLevel(10)
+	--OmenText:SetTemplate('Default', true)
+	
+	AtlasText.text = AtlasText:CreateFontString(nil, 'OVERLAY')
+	AtlasText.text:FontTemplate()
+	AtlasText.text:SetPoint('CENTER')
+	AtlasText.text:SetJustifyH('CENTER')
+	AtlasText.text:SetText(E.ValColor..'Atlas')
+	
+	E.CreateMover(anchor, AtlasText, "AtlasTextMover", "aMOVE")
+	
+	AtlasText:SetScript("OnMouseDown", function(self, btn)
+		if btn == "RightButton" 
+			then 
+				AtlasLoot:OptionsToggle()
+			else
+				AtlasLoot:ShowFrame_MiniMap()
+		end
+	end)
+	
+	if IsAddOnLoaded("ElvUI") then
+		local x = CreateFrame("Frame")
+		x:RegisterEvent("PLAYER_ENTERING_WORLD")
+		x:SetScript("OnEvent", function(self, event)
+		if not Recount then return end
+			RightChatPanel:HookScript("OnShow", function() AtlasText:Show(); end)
+			RightChatPanel:HookScript("OnHide", function()  AtlasText:Hide(); end)
+			self:UnregisterAllEvents()
+		end)
+	end
+	
