73d846b7dfe5ea3852000b82a2b84720e752c31d
Thunx <friberg_mattias@hotmail.com>
2012-02-04 15:39:55 +0100

    Forgot to add the file...

---
+++ ElvUI_Thunx/modules/misc/combatallert.lua
@@ -0,0 +1,168 @@
+local E, L, DF = unpack(ElvUI); -- Import Functions/Constants, Config, Locales
+-------------------------------------------------------------------------------------
+-- Credit Alleykat 
+-- Entering combat and allertrun function (can be used in anther ways)
+------------------------------------------------------------------------------------
+
+local speed = .041 -- how fast the text appears
+ 
+local GetNextChar = function(word,num)
+	local c = word:byte(num)
+	local shift
+	if not c then return "",num end
+		if (c > 0 and c <= 127) then
+			shift = 1
+		elseif (c >= 192 and c <= 223) then
+			shift = 2
+		elseif (c >= 224 and c <= 239) then
+			shift = 3
+		elseif (c >= 240 and c <= 247) then
+			shift = 4
+		end
+	return word:sub(num,num+shift-1),(num+shift)
+end
+function Thunx_DoWork_Combatallert() 
+local updaterun = CreateFrame("Frame")
+
+local flowingframe = CreateFrame("Frame",nil,UIParent)
+	flowingframe:SetFrameStrata("HIGH")
+	flowingframe:SetPoint("CENTER", 0, 170) -- where we want the textframe
+	flowingframe:SetHeight(64)
+	flowingframe:SetWidth(500)	
+	E.CreateMover(anchor, flowingframe, "Combatallert", "combatallert")
+
+local flowingtext = flowingframe:CreateFontString(nil,"OVERLAY")
+	local db = E.db.skins.thunx 
+	flowingtext:FontTemplate(E["media"].fdFont, 20, db.fdfontflags)
+	flowingtext:SetShadowOffset(1,-1)
+ 
+local rightchar = flowingframe:CreateFontString(nil,"OVERLAY")
+	local db = E.db.skins.thunx 
+	rightchar:FontTemplate(E["media"].fdFont, db.fdfontsize, db.fdfontflags)
+	rightchar:SetShadowOffset(1,-1)
+	rightchar:SetJustifyH("LEFT") -- left or right
+ 
+local count,len,step,word,stringE,a,backstep
+ 
+local nextstep = function()
+	a,step = GetNextChar (word,step)
+	flowingtext:SetText(stringE)
+	stringE = stringE..a
+	a = string.upper(a)
+	rightchar:SetText(a)
+end
+
+local backrun = CreateFrame("Frame")
+backrun:Hide()
+
+local updatestring = function(self,t)
+	count = count - t
+		if count < 0 then
+			count = speed
+			if step > len then
+				self:Hide()
+				flowingtext:SetText(stringE)
+				rightchar:SetText()
+				flowingtext:ClearAllPoints()
+				flowingtext:SetPoint("RIGHT")
+				flowingtext:SetJustifyH("RIGHT")
+				rightchar:ClearAllPoints()
+				rightchar:SetPoint("RIGHT",flowingtext,"LEFT")
+				rightchar:SetJustifyH("RIGHT")
+				self:Hide()
+				count = 1.8
+				backrun:Show()
+			else
+				nextstep()
+			end
+		end
+end
+ 
+updaterun:SetScript("OnUpdate",updatestring)
+updaterun:Hide()
+
+local backstepf = function()
+	local a = backstep
+	local firstchar
+		local texttemp = ""
+		local flagon = true
+		while a <= len do
+			local u
+			u,a = GetNextChar(word,a)
+			if flagon == true then
+				backstep = a
+				flagon = false
+				firstchar = u
+			else
+				texttemp = texttemp..u
+			end
+		end
+	flowingtext:SetText(texttemp)
+	firstchar = string.upper(firstchar)
+	rightchar:SetText(firstchar)
+end
+ 
+local rollback = function(self,t)
+	count = count - t
+		if count < 0 then
+			count = speed
+			if backstep > len then
+				self:Hide()
+				flowingtext:SetText()
+				rightchar:SetText()
+			else
+				backstepf()
+			end
+		end
+end
+ 
+backrun:SetScript("OnUpdate",rollback)
+ 
+local allertrun = function(f,r,g,b)
+	flowingframe:Hide()
+	updaterun:Hide()
+	backrun:Hide()
+ 
+	flowingtext:SetText(f)
+	local l = flowingtext:GetWidth()
+ 
+	local color1 = r or 1
+	local color2 = g or 1
+	local color3 = b or 1
+ 
+	flowingtext:SetTextColor(color1*.95,color2*.95,color3*.95) -- color in RGB(red green blue)(alpha)
+	rightchar:SetTextColor(color1,color2,color3)
+ 
+	word = f
+	len = f:len()
+	step,backstep = 1,1
+	count = speed
+	stringE = ""
+	a = ""
+ 
+	flowingtext:SetText("")
+	flowingframe:SetWidth(l)
+	flowingtext:ClearAllPoints()
+	flowingtext:SetPoint("LEFT")
+	flowingtext:SetJustifyH("LEFT")
+	rightchar:ClearAllPoints()
+	rightchar:SetPoint("LEFT",flowingtext,"RIGHT")
+	rightchar:SetJustifyH("LEFT")
+ 
+	rightchar:SetText("")
+	updaterun:Show()
+	flowingframe:Show()
+end
+  
+local a = CreateFrame ("Frame")
+	a:RegisterEvent("PLAYER_REGEN_ENABLED")
+	a:RegisterEvent("PLAYER_REGEN_DISABLED")
+	a:SetScript("OnEvent", function (self,event)
+		if (UnitIsDead("player")) then return end
+		if event == "PLAYER_REGEN_ENABLED" then
+			allertrun(LEAVING_COMBAT,0.1,1,0.1)
+		else
+			allertrun(ENTERING_COMBAT,1,0.1,0.1)
+		end
+	end)
+end	
